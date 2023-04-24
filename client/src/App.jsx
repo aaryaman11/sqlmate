07ed03d3f1c318197b135f5ceed287b82d1c0117
Aaryaman <aaryaman.sh@gmail.com>
2023-04-24 14:49:29 -0700

    onSubmit is updating the state of querryDescription and printing the value

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,16 @@ import { useState } from 'react'
 function App() {
   const [querryDescerption, setQuerryDescerption] = useState('')
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log("form submitted:", querryDescerption)
+  }
+
   return (
     <main className={styles.main}>
       <img src={sqlLogo} alt="sqlLogo" className={styles.icon} />
       <h3>Generate SQL with AI</h3>
-      <form action="/" method="post">
+      <form onSubmit={onSubmit}>
         <input type="text" placeholder="Create your query" name="Query-description" onChange={(e) => setQuerryDescerption(e.target.value)} />
         <input type="submit" value='Generate querry' />
       </form>
