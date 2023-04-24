a4e2fb069602d538a07542a2ade87b4fc4a5e400
Aaryaman <aaryaman.sh@gmail.com>
2023-04-24 15:52:36 -0700

    index.js is working annd listening to port 3005

--- server/index.js
+++ server/index.js
@@ -3,3 +3,13 @@ import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
+
+const PORT = process.env.PORT || 3005;
+
+app.get('/', (req, res) => {
+    res.send('Hello World from the API!');
+});
+app.listen(PORT, () => 
+    {console.log(`Server running on port:${PORT}`)
+});
