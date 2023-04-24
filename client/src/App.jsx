470b318bbbbef72341a26f8f5d6102a23c6b29c4
Aaryaman <aaryaman.sh@gmail.com>
2023-04-24 14:33:55 -0700

    imported sql logo and making form taking the input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import styles from './index.module.css'
+import sqlLogo from './assets/sql-logo.png'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <h1>hellow world</h1>
+    <main className={styles.main}>
+      <img src={sqlLogo} alt="sqlLogo" className={styles.icon} />
+      <h3>Generate SQL with AI</h3>
+
+
+    </main>
   )
 }
 
