4ecc1d9671ef542b97ff15b9ec7142eff5d34138
Aaryaman <aaryaman.sh@gmail.com>
2023-04-24 14:45:04 -0700

    integrated state to update query description

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import styles from './index.module.css'
 import sqlLogo from './assets/sql-logo.png'
 
+import { useState } from 'react'
+
 
 function App() {
-  
+  const [querryDescerption, setQuerryDescerption] = useState('')
 
   return (
     <main className={styles.main}>
       <img src={sqlLogo} alt="sqlLogo" className={styles.icon} />
       <h3>Generate SQL with AI</h3>
-
+      <form action="/" method="post">
+        <input type="text" placeholder="Create your query" name="Query-description" onChange={(e) => setQuerryDescerption(e.target.value)} />
+        <input type="submit" value='Generate querry' />
+      </form>
 
     </main>
   )
